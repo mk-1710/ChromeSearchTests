593272eafaf7059b089fe9e87b20573732b23125
mk-1710 <kurmanovm@gmail.com>
2020-12-23 20:51:11 +0500

    Build 0.1

--- src/test/java/ChromeSearchTests.java
+++ src/test/java/ChromeSearchTests.java
@@ -2,7 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class ChromeSearchTests {
 
@@ -11,71 +17,79 @@ public class ChromeSearchTests {
     //3. type the query
     //4. submit the query
     //5. verify the result page
+    //we will use google and yahoo search engines
 
 
     WebDriver driver;
     @Test
     public void testYahooSearch() {
         //yahoo search engine has a 'search button' which is clickable
+        //but used submit method of WebElement
+
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/windows32/chromedriver.exe");
 
-        openBrowser();
-        navigateToYahoo();
-        typeAndSubmitYahooQuery();
-        //submitYahooQuery();
-        verifyYahooResultPage();
+        openChromeBrowser();
+        navigateToSearchEngine("http://yahoo.com");
+        typeAndSubmitQuery("//input[@name='p']", "Portnov computer school");
+        boolean linkIsFound = verifyResultPage("//a[contains(text(), 'Portnov Computer School - Software')]");
+
+        if(linkIsFound){
+            System.out.println("Link in Yahoo search is successfully found!");
+        }else{
+            System.out.println("Link not found in Yahoo search!");
+        }
+        driver.close();
+        Assert.assertTrue(linkIsFound);
     }
 
     @Test
     public void testGoogleSearch() {
-
         //google search engine does not have 'search button'
-        //use
+        //used submit method of WebElement
+
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/windows32/chromedriver.exe");
-        openBrowser();
-        navigateToGoogle();
-        typeAndSubmitGoogleQuery();
+        openChromeBrowser();
+        navigateToSearchEngine("https://www.google.com");
+        typeAndSubmitQuery("//input[@name='q']", "Portnov computer school");
+
+        boolean linkIsFound = verifyResultPage("//span[contains(text(), 'Portnov Computer School - YouTube')]");
+
+        if(linkIsFound){
+            System.out.println("Link in Google search is successfully found!");
+        }else{
+            System.out.println("Link not found in Google search!");
+        }
+        driver.close();
+
+        Assert.assertTrue(linkIsFound);
+    }
+
+    public boolean verifyResultPage(String objectXPath) {
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        try {
+            WebElement linkToFind = driver.findElement(By.xpath(objectXPath));
+            return true;
+        }catch (Exception MyException){
+            return false;
+        }
 
     }
 
-    private void verifyYahooResultPage() {
-        String textXPath = "Portnov Computer School - Software";
-        //String linkXPath = "//*[text(), 'Portnov Computer School - Software Testing and Software QA ...')]";
-        String linkXPath = "//a[@class=' ac-algo fz-l ac-21th lh-24']";
-
-        //WebElement findAnchor = driver.findElement(By.partialLinkText("Port"));
-        WebElement findAnchor = driver.findElement(By.xpath(linkXPath));
-        String textMyOne = findAnchor.getText();
-        System.out.println(textMyOne);
-    }
-
-     private void typeAndSubmitYahooQuery() {
-        String searchFieldXPath = "//input[@name='p']";
-        String whatToFind = "Portnov computer school";
+    private void typeAndSubmitQuery(String searchFieldXPath, String whatToFind) {
 
         WebElement searchField = driver.findElement(By.xpath(searchFieldXPath));
         searchField.sendKeys(whatToFind);
         searchField.submit();
+
     }
 
-    private void typeAndSubmitGoogleQuery() {
-        String searchFieldsXPath = "//input[@name='q']";
-        String whatToFind = "Portnov computer school";
-
-        WebElement searchField = driver.findElement(By.xpath(searchFieldsXPath));
-        searchField.sendKeys(whatToFind);
-        searchField.submit();
+    private void navigateToSearchEngine(String searchEngineURL) {
+        driver.get(searchEngineURL);
     }
 
-    private void navigateToGoogle() {
-        driver.get("https://www.google.com");
-    }
-
-    private void navigateToYahoo() {
-        driver.get("http://yahoo.com");
-    }
-
-    private void openBrowser() {
+    private void openChromeBrowser() {
         driver = new ChromeDriver();
     }
 }
